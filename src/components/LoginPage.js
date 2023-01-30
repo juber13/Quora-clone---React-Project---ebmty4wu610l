62b4e1a09e7e1da965b20202a863405bfe2786c7
juber <juberkhan0707@gmail.com>
2023-01-30 14:32:59 +0530

    make authetication

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,5 @@
 import React,{useState} from "react";
 import { useNavigate } from "react-router-dom";
-import '../styles/Login.css';
 
 function getUserDate(){
   const userDate = localStorage.getItem('userData');
